bac1097856fbec7ad5886a3aa265a80a547893d9
tanaphon123 <tanaphonintachai@gmail.com>
2023-04-03 21:17:55 +0700

    แก้ฟังก์ชัน

--- test.js
+++ test.js
@@ -1,9 +1,5 @@
 const prime = (number) => {
-    let data = []
-    for (let i=1; i<=number; i++) {
-        i === 1 ? "" : i%2 === 0 ? "" : i%i === 0 ? data.push(i) : ""
-    } const result = `เลขจำนวนเฉพาะของ ${number} มีจำนวนทั้งหมด ${data.length} ${data.length === 0 ? ``:`\nและมีจำนวนดังนี้ ${data}`}`
-    return result; 
+    console.log("เสือ")
 }
 
 console.log(prime(100));
